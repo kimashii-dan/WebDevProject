e150dc99db6952b6df05096fc8a71f9a5d67905d
balnur <balnur_30_04_06@mail.ru>
2025-04-23 23:23:04 +0500

    everything works

--- frontend/src/app/components/profile/profile.service.ts
+++ frontend/src/app/components/profile/profile.service.ts
@@ -15,3 +15,31 @@ export class ProfileService {
     return this.httpClient.get<UserData>(this.url);
   }
 }
+
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { User } from '../models/user.model';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ProfileService {
+//   private apiUrl = 'http://localhost:8000/api/profile';  // Django API URL
+
+//   constructor(private http: HttpClient) {}
+
+//   getProfile(): Observable<User> {
+//     return this.http.get<User>(`${this.apiUrl}/`);
+//   }
+
+//   updateProfile(user: User): Observable<User> {
+//     return this.http.patch<User>(`${this.apiUrl}/update/`, user);
+//   }
+
+//   uploadAvatar(file: File): Observable<{ avatarUrl: string }> {
+//     const formData = new FormData();
+//     formData.append('avatar', file);
+//     return this.http.post<{ avatarUrl: string }>(`${this.apiUrl}/upload_avatar/`, formData);
+//   }
+// }
